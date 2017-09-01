e58f0cc134fc539ab587441b33cf2c4d3d4bab9a
Niles Rogoff <me@niles.xyz>
2017-09-01 19:45:44 -0400

    Pull date last bumped from modify time of the file

--- file_importer.py
+++ file_importer.py
@@ -1,10 +1,10 @@
-import glob, subprocess, pymysql, html, os
+import glob, subprocess, pymysql, html, os, time
 def escape(x): return con.escape_string(x)
-def insert_op(title, comment, board):
+def insert_op(title, comment, board, date):
 	comment = escape(html.unescape(comment))
 	title = escape(html.unescape(title))
 	res = "DELETE FROM x;\n"
-	res += "INSERT INTO posts (board, title, content) VALUES ('"+board+"', '"+title+"', '"+comment+"');\n"
+	res += "INSERT INTO posts (board, title, content, date_posted, last_bumped) VALUES ('"+board+"', '"+title+"', '"+comment+"', TIMESTAMP('"+date+"'), TIMESTAMP('"+date+"'));\n"
 	res += "INSERT INTO x (id) VALUES (LAST_INSERT_ID());\n"
 	return res
 def insert_reply(comment, board):
@@ -18,6 +18,7 @@ out.write("CREATE TEMPORARY TABLE x (id INTEGER);\n")
 for board in glob.glob("*"):
 	if not os.path.isdir(board): continue
 	for f in glob.glob(board + "/thread/*.txt"):
+		date = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(os.stat(f).st_mtime))
 		f = open(f, "r").read().split("\n")
 		title = f[0][3:]
 		f[1] = "#" + f[1][2:]
@@ -32,7 +33,7 @@ for board in glob.glob("*"):
 			else:
 				reply += "\n" + line
 		replies.append(reply)
-		out.write(insert_op(title, replies[1], board))
+		out.write(insert_op(title, replies[1], board, date))
 		for reply in replies[2:]:
 			out.write(insert_reply(reply, board))
 out.write("DROP TABLE x;\n")
